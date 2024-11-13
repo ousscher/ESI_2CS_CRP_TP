606c3f558ab63081ac0fd346e9eb9e9b343b7f22
ousscher <lo_cherguelaine@esi.dz>
2024-11-13 01:54:37 +0100

    finish with tp (code part)

--- TP1/main.c
+++ TP1/main.c
@@ -44,21 +44,14 @@ int main() {
         clock_t fin1 = clock();
         double temps_execution1 = (double)(fin1 - debut1) / CLOCKS_PER_SEC;
         
-        // printf("Algorithme 1 - Le nombre %lld est %s. Temps d'exécution: %f secondes\n", 
-        //        valeurs[i], resultat1 ? "premier" : "non premier", temps_execution1);
-
         clock_t debut2 = clock();
         bool resultat2 = algo2(valeurs[i]);
         clock_t fin2 = clock();
         double temps_execution2 = (double)(fin2 - debut2) / CLOCKS_PER_SEC;
-        // printf("Algorithme 2 - Le nombre %lld est %s. Temps d'exécution: %f secondes\n", 
-        //        valeurs[i], resultat2 ? "premier" : "non premier", temps_execution2);
         clock_t debut3 = clock(); 
         bool resultat3 = algo3(valeurs[i]); 
         clock_t fin3 = clock(); 
         double temps_execution3 = (double)(fin3 - debut3) / CLOCKS_PER_SEC; 
-        // printf("Algorithme 3  - Le nombre %lld est %s. Temps d'exécution: %f secondes\n\n", 
-        //        valeurs[i], resultat3 ? "premier" : "non premier", temps_execution3);
 
         long long am =( temps_execution1 - temps_execution3) / temps_execution1 * 100; 
         printf("| %11lld |    %s    | %9f | %9f | %9f |    %4lld%%     |\n", valeurs[i],resultat3 ? "Y" : "N",temps_execution1, temps_execution2, temps_execution3, am ); 
